eb17f965b0c1a66b93d8f532102a362ad1b9f420
Jesús Manuel Martinez <99193827+Yisus538@users.noreply.github.com>
2023-11-26 18:46:05 -0300

    Actualizacion

--- Estructuras Repetitivas/do while.cpp
+++ Estructuras Repetitivas/do while.cpp
@@ -1,15 +1,14 @@
 //actua y despues piensa
 #include <iostream>
 #include <stdlib.h>
-using namespace std;
 
 int main(){
     int i=1;
 
     do{
-      cout<<i<<endl;
+      std::cout<<i<<std::endl;
       i++;
-    }while(i<=10);
+    }while(i<=10); 
     
     system("pause");
     return 0;
